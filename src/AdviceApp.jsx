f49500cc9d2a3a8a3a1767538e3f1b107419c08c
Aishwarya <akaishwarya88@gmail.com>
2024-12-10 17:08:53 +0530

    React-Advice-App react to github

--- src/AdviceApp.jsx
+++ src/AdviceApp.jsx
@@ -3,7 +3,7 @@ import './AdviceApp.css'
 
 export const AdviceApp = () => {
     const [advice, setAdvice] = useState("Please Click Button to Get Advice ");
-    const [count, setCount] = useState(-1);
+    const [count, setCount] = useState(1);
 
     async function getAdvice() {
         const res = await fetch("https://api.adviceslip.com/advice");
